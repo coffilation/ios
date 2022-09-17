558dfd2bbf40dac5eca7f05a0252807b4ed89ac8
Matvei Borisov <bme15122000@gmail.com>
2022-09-18 02:29:08 +0300

    init application

---
+++ coffilation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coffilation
+//
+//  Created by Матвей Борисов on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
