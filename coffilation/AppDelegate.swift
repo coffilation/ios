9f9de3b52de29b88d79d5f24f916a61da4430abd
Matvei Borisov <bme15122000@gmail.com>
2022-10-10 01:42:18 +0300

    delete storyboard from project and fix issues

--- coffilation/AppDelegate.swift
+++ coffilation/AppDelegate.swift
@@ -9,15 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-	
-	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		return true
 	}
-	
+
 	// MARK: UISceneSession Lifecycle
-	
+
 	func application(_ application: UIApplication,
 					 configurationForConnecting connectingSceneSession: UISceneSession,
 					 options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -25,8 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Use this method to select a configuration to create the new scene with.
 		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
-	
+
 	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 	}
-	
+
 }
