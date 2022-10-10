1a2f1728eb4d7ad6b23d0183bcd9d21b43e77c2c
Matvei Borisov <bme15122000@gmail.com>
2022-10-11 00:58:59 +0300

    create auth manager base class

---
+++ coffilation/AuthManager.swift
@@ -0,0 +1,16 @@
+//
+//  AuthManager.swift
+//  coffilation
+//
+//  Created by Матвей Борисов on 11.10.2022.
+//
+
+import Foundation
+
+protocol AuthManagerProtocol {
+
+}
+
+class AuthManager: AuthManagerProtocol {
+
+}
