0867c0a076bca8f52f145179dfc7c9dd564e83fa
Matvei Borisov <bme15122000@gmail.com>
2022-10-31 02:15:58 +0300

    make network manager

---
+++ coffilation/NetworkManager.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkManager.swift
+//  coffilation
+//
+//  Created by Матвей Борисов on 16.10.2022.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+	case noResponse
+	case decodeFailure
+	case notAuthorized
+}
+
+protocol NetworkManagerProtocol {
+	typealias RequestCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void
+
+	@discardableResult func request<T: Decodable>(with url: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> RequestCompletion
+
+	func retryRequest(for request: URLRequest, completion: @escaping RequestCompletion)
+}
+
+class NetworkManager: NetworkManagerProtocol {
+	private let session = URLSession(configuration: .default)
+
+	func request<T: Decodable>(with request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> RequestCompletion {
+		let requestCompletion = { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+			if let error {
+				completion(.failure(error))
+				return
+			}
+
+			if let response = response as? HTTPURLResponse {
+				if response.statusCode == 401 {
+					completion(.failure(NetworkError.notAuthorized))
+				}
+			}
+			guard let data = data else {
+				completion(.failure(NetworkError.noResponse))
+				return
+			}
+			guard let responseObject = try? JSONDecoder().decode(T.self, from: data) else {
+				completion(.failure(NetworkError.decodeFailure))
+				return
+			}
+			completion(.success(responseObject))
+
+		}
+		let dataTask = session.dataTask(with: request, completionHandler: requestCompletion)
+		dataTask.resume()
+		return requestCompletion
+	}
+
+	func retryRequest(for request: URLRequest, completion: @escaping RequestCompletion) {
+		let dataTask = session.dataTask(with: request, completionHandler: completion)
+		dataTask.resume()
+	}
+}
