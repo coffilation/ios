f69d6c095ac0802594ce80b82e95a0921e87146b
Matvei Borisov <bme15122000@gmail.com>
2022-10-31 02:17:17 +0300

    done auth manager for token refreshing flow

--- coffilation/AuthManager.swift
+++ coffilation/AuthManager.swift
@@ -7,10 +7,109 @@
 
 import Foundation
 
-protocol AuthManagerProtocol {
+struct AuthLoginResponseModel: Codable {
+	let access: String
+	let refresh: String
+}
 
+struct AuthLoginRequestModel: Encodable {
+	let username: String
+	let password: String
+}
+
+struct AuthRefreshRequestModel: Encodable {
+	let refresh: String
+}
+
+enum AuthError: Error {
+	case responseError
+}
+
+protocol AuthManagerProtocol {
+	func performLogin(email: String, password: String, completion: @escaping (AuthError?) -> Void)
+
+	func authorizedRequest<T: Decodable>(with request: URLRequest, completion: @escaping (Result<T, Error>) -> Void)
 }
 
 class AuthManager: AuthManagerProtocol {
+	private let networkManager: NetworkManagerProtocol
+	private let tokenManager: TokenManagerProtocol
+	private let lock = NSLock()
+	private var needRefreshRequests = [URLRequest: NetworkManagerProtocol.RequestCompletion]()
 
+	init(networkManager: NetworkManagerProtocol, tokenManager: TokenManagerProtocol) {
+		self.networkManager = networkManager
+		self.tokenManager = tokenManager
+	}
+
+	func performLogin(email: String, password: String, completion: @escaping (AuthError?) -> Void) {
+		let body = AuthLoginRequestModel(username: email, password: password)
+
+		guard let request = try? RequestBuilder(path: "auth/login")
+			.httpMethod(.post)
+			.httpHeader(name: "Content-Type", value: "application/json")
+			.httpJSONBody(body)
+			.makeRequestForCofApi() else {
+			return
+		}
+
+		networkManager.request(with: request) { [weak self] (result: Result<AuthLoginResponseModel, Error>) in
+			switch result {
+			case .success(let model):
+				self?.tokenManager.saveTokens(with: model)
+				completion(nil)
+			case .failure(_):
+				completion(AuthError.responseError)
+			}
+		}
+	}
+
+	func authorizedRequest<T: Decodable>(with request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) {
+		guard let enrichedRequest = tokenManager.enrichAuthorizedRequest(request) else {
+			return
+		}
+
+		let requestCompletion = networkManager.request(with: enrichedRequest) { [weak self] (result: Result<T, Error>) in
+			switch result {
+			case .success(let success):
+				self?.lock.lock()
+				self?.needRefreshRequests[request] = nil
+				self?.lock.unlock()
+				completion(.success(success))
+			case .failure(let failure as NetworkError):
+				if failure == .notAuthorized {
+					self?.tokenManager.updateToken()
+				}
+			case .failure(let failure):
+				self?.lock.lock()
+				self?.needRefreshRequests[request] = nil
+				self?.lock.unlock()
+				completion(.failure(failure))
+			}
+		}
+		lock.lock()
+		needRefreshRequests[request] = requestCompletion
+		lock.unlock()
+
+	}
+
+}
+
+extension AuthManager: TokenManagerDelegate {
+	func didUpdateToken() {
+		lock.lock()
+		for requestTuple in needRefreshRequests {
+			guard let enrichedRequest = tokenManager.enrichAuthorizedRequest(requestTuple.key) else {
+				return
+			}
+			networkManager.retryRequest(for: enrichedRequest,
+										completion: requestTuple.value)
+		}
+		needRefreshRequests = [:]
+		lock.unlock()
+	}
+
+	func didReceiveTokenUpdateError() {
+		needRefreshRequests = [:]
+	}
 }
--- coffilation/DependencyContainer.swift
+++ coffilation/DependencyContainer.swift
@@ -11,8 +11,35 @@ protocol HasAuthManager {
 	var authManager: AuthManagerProtocol { get set }
 }
 
-protocol DependencyContainerProtocol: HasAuthManager {}
+protocol HasNetworkManager {
+	var networkManager: NetworkManagerProtocol { get set }
+}
+
+protocol HasTokenProvider {
+	var tokenProvider: TokenProviderProtocol { get set }
+}
+
+protocol HasTokenManager {
+	var tokenManager: TokenManagerProtocol { get set }
+}
+
+protocol DependencyContainerProtocol: HasAuthManager,
+									  HasNetworkManager,
+									  HasTokenProvider,
+									  HasTokenManager {}
 
 final class DependencyContainer: DependencyContainerProtocol {
-	var authManager: AuthManagerProtocol = AuthManager()
+	var authManager: AuthManagerProtocol
+	var tokenManager: TokenManagerProtocol
+
+	var networkManager: NetworkManagerProtocol = NetworkManager()
+	var tokenProvider: TokenProviderProtocol = TokenProvider()
+
+	init() {
+		let tokenManager = TokenManager(tokenProvider: tokenProvider, networkManager: networkManager)
+		self.tokenManager = tokenManager
+		let authManager = AuthManager(networkManager: networkManager, tokenManager: tokenManager)
+		self.authManager = authManager
+		tokenManager.delegate = authManager
+	}
 }
