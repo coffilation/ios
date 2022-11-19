894704cc7aea095986699b245d94d2270781998a
Matvei Borisov <bme15122000@gmail.com>
2022-11-19 22:44:55 +0300

    add entity for colors and icons

---
+++ coffilation/CoffilationColor.swift
@@ -0,0 +1,20 @@
+//
+//  CoffilationColor.swift
+//  coffilation
+//
+//  Created by Матвей Борисов on 15.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+	static let mainColor = UIColor(red: 255/255, green: 103/255, blue: 72/255, alpha: 1)
+
+	static let darkGray = UIColor(red: 108/255, green: 108/255, blue: 108/255, alpha: 1)
+
+	static let grey2 = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+
+	static let defaultBackground = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+
+	static let defaultButtonText = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+}
---
+++ coffilation/CoffilationImage.swift
@@ -0,0 +1,20 @@
+//
+//  CoffilationImage.swift
+//  coffilation
+//
+//  Created by Матвей Борисов on 15.11.2022.
+//
+
+import UIKit
+
+struct CoffilationImage {
+	static let logo = UIImage(named: "Logo")
+
+	static let lock = UIImage(named: "LockIcon")
+
+	static let login = UIImage(named: "LoginIcon")
+
+	static let profile = UIImage(named: "ProfileIcon")
+
+	static let hidePassword = UIImage(named: "HidePasswordIcon")
+}
